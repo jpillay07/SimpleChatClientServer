7d7091274cbcfb7ea299f7dfea22a5dd34b27bb7
Justin Pillay <jpillay07@gmail.com>
2020-10-24 18:15:49 +0200

    first commit

---
+++ SimpleChatClientA.java
@@ -0,0 +1,111 @@
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SimpleChatClientA{
+    JTextArea incoming;
+    JTextField outgoing;
+    BufferedReader reader;
+    PrintWriter writer;
+    Socket sock;
+
+    public static void main(String[] args){
+        new SimpleChatClientA().go();
+    }
+
+    public void go(){
+        JFrame frame = new JFrame("Ludicrously Simple Chat Client");
+        JPanel mainPanel = new JPanel();
+
+        incoming = new JTextArea(15,20);
+        incoming.setLineWrap(true);
+        incoming.setWrapStyleWord(true);
+        incoming.setEditable(false);
+
+        JScrollPane qScroller = new JScrollPane(incoming);
+        qScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        qScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
+        outgoing = new JTextField(20);
+
+        JButton sendButton = new JButton("Send");
+        sendButton.addActionListener(new SendButtonListener());
+
+        mainPanel.add(qScroller);
+        mainPanel.add(outgoing);
+        mainPanel.add(sendButton);
+
+        setUpNetworking();
+
+        Thread readerThread = new Thread(new IncomingReader());
+        readerThread.start();
+
+        frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
+        frame.setSize(400, 500);
+        frame.setVisible(true);
+
+    }
+
+    private void setUpNetworking(){
+        try{
+            sock = new Socket("127.0.0.1", 5000);
+            InputStreamReader streamReader = new InputStreamReader(sock.getInputStream());
+            reader = new BufferedReader(streamReader);
+            writer = new PrintWriter(sock.getOutputStream());
+            System.out.println("Networking Established");
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+        
+
+    }
+
+    public class IncomingReader implements Runnable{
+
+        @Override
+        public void run() {
+            String message;
+            try{
+                while((message = reader.readLine()) != null){
+                    System.out.println(("read " + message));
+                    incoming.append(message + "\n");
+                }
+            }catch(Exception ex){
+                ex.printStackTrace();
+            }
+            
+
+        }
+        
+    }
+
+    public class SendButtonListener implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            try{
+                writer.println(outgoing.getText());
+                writer.flush();
+            }catch(Exception ex){
+                ex.printStackTrace();
+            }
+
+            outgoing.setText("");
+            outgoing.requestFocus();
+        }
+        
+    }
+}
